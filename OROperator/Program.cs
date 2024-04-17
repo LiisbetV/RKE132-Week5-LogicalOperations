81daa3569bcd2c320377834a57308845b575e53d
Liisbet V. <liisbet.vaiksaar@tktk.ee>
2024-04-17 12:55:01 +0300

    add week5logicaloperations code

---
+++ OROperator/Program.cs
@@ -0,0 +1,21 @@
+﻿//Math >= 90 OR Chemistry >= 90
+
+int math, biology, chemistry;
+
+Console.WriteLine("Enter your Math result: ");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Biology result: ");
+biology = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Eneter your Chemistry result: ");
+chemistry = Int32.Parse(Console.ReadLine());
+
+if((math >= 90 && chemistry >= 90) || (math >= 90 && biology >= 90) || (chemistry >= 90 && biology >= 90))
+{
+    Console.WriteLine("Congratualtions! You got accepted!");
+}
+else
+{
+    Console.WriteLine("Your application cannot be approved.");
+}
